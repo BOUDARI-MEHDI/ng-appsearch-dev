329e0fee6ba17f46a8116f50f97a9bc8026f83aa
BOUDARI-MEHDI <75070663+BOUDARI-MEHDI@users.noreply.github.com>
2022-11-30 16:50:45 +0100

    1 file modified

--- src/app/search/search.config.ts
+++ src/app/search/search.config.ts
@@ -4,13 +4,9 @@ import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector';
 // @ts-ignore
 const connector = new AppSearchAPIConnector({
   /* MH connector */
-  searchKey: 'search-snsuo76iiedkkbx5deouk1xk',
-  engineName: 'search-qpm-epargne',
-  //endpointBase: 'https://entreprise-search-bot.recf.elk.si2m.tec',
-  hostIdentifier: 'https://entreprise-search-bot.recf.elk.si2m.tec',
-  /*searchKey: 'search-snsuo76iiedkkbx5deouk1xk',
+  searchKey: 'search-gpt7wpsxo5sp8js4q2e6tzth',
   engineName: 'search-epargne',
-  endpointBase: 'https://entreprise-search-bot.recf.elk.si2m.tec', */
+  endpointBase: 'https://entreprise-search-bot.recf.elk.si2m.tec',
 });
 
 const config = {
@@ -35,41 +31,9 @@ const config = {
     facets: {
       grand_compte: { type: 'value' },
       origine: { type: 'value' },
-      author: { type: 'value' },
-      extension: { type: 'value' },
       page: { type: 'value' },
-      creation_date: {
-        type: 'range',
-        ranges: [
-          {
-            from: '2020-01-01T01:01:33.420Z',
-            name: 'Les anciens documents',
-          },
-          {
-            from: '2021-01-01T01:01:33.420Z',
-            to: '2021-12-31T23:59:33.420Z',
-            name: 'Les documents en 2021',
-          },
-          {
-            to: '2022-01-01T01:01:33.420Z',
-            name: 'Les documents de 2022',
-          },
-        ],
-      },
-      modification_date: {
-        type: 'range',
-        ranges: [
-          {
-            from: '2021-01-01T01:01:33.420Z',
-            to: '2021-12-31T23:59:33.420Z',
-            name: 'Document modifié en 2021',
-          },
-          {
-            to: '2022-01-01T01:01:33.420Z',
-            name: 'Document modifié cette année',
-          },
-        ],
-      },
+      creation_date: { type: 'value' },
+      modification_date: { type: 'value' },
     },
   },
   autocompleteQuery: {
